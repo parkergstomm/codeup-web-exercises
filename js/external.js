cc51bd1024afcb2066aa4dbe4c180649d92e37fd
Parker Stomm <parker.g.stomm@gmail.com>
2023-01-20 15:11:34 -0500

    first git push

---
+++ js/external.js
@@ -0,0 +1,29 @@
+// console.log << puts a message in the web console
+console.log("Can you see me in the Console?");
+
+//Alert - How do I first open up a window to show my user some information
+alert("This is an alert!");
+
+//Can also do variables :D
+var myName = "Parker";
+
+alert("Hey, my name is " + myName);
+
+//Can it run an expression?
+
+alert(2+2); // YES!
+
+//Alert - really just shoots out information to read
+
+
+//Confirm - True or false [ok and cancel]
+var likedLunch = confirm("How was your lunch today? Did you like your lunch?");
+
+alert("You told me that it was " + likedLunch + " That you liked lunch");
+
+//Prompt - can you write me something in the response to what i asked in the window?
+
+var whatDidYouHaveForLunch = prompt("Hey, I know it was " + likedLunch + " that you liked lunch, but what did you have?");
+
+alert("You told me you had " + whatDidYouHaveForLunch + " for lunch");
+
