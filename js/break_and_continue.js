a28b0368358c143ed7de15803c8ad02fc108cca0
Parker Stomm <parker.g.stomm@gmail.com>
2023-01-26 18:09:40 -0500

    Not much done in break-continue exercises. Coming back later to work on them some more.

---
+++ js/break_and_continue.js
@@ -0,0 +1,7 @@
+//Break and Continue Exercises
+
+//1. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+
+//[] prompt the user for an Odd number between 1 -50
+//[] use a loop and BREAK statement to CONTINUE prompting user IF THEY ENTER invalid input
+
