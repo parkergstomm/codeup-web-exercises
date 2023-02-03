277a335e44182fa9746549d84fa892d0d981f236
Parker Stomm <parker.g.stomm@gmail.com>
2023-02-03 18:28:33 -0500

    Working on Fuction drills

--- js/function_drills.js
+++ js/function_drills.js
@@ -1,33 +1,69 @@
-function isOdd(number){
-    return number % 2 === 1;
+
+
+
+// Define a function named timesFour that takes in an input and multiples that input by four if it is numeric. If the input is not numeric, then return false.
+// timesFour(0) //0
+// timesFour("5") //20
+// timesFour(-5) //-20
+// timesFour('Texas') //false
+// timesFour([2,1,0]) //false
+// timesFour(true) //false
+// timesFour(null) //false
+
+// [x] define(create) a function that has a parameter named timesFour
+// [x] has to multiply the numeric by 4 / false if value is not numeric
+// [] commit to git
+
+// function timesFour(input){
+//     if(!isNaN(input) || typeof input !== "string"){
+//         return input * 4;
+//     } else {
+//         return false;
+//
+//     }
+// }
+
+function timesFour(input){
+    if(!isNaN(parseInt(input))){ // if the parseInt of input is the opposite of isNaN - do the return
+        return input * 4;
+    } else { // if not - return false
+        return false;
+    }
 }
 
-function isEven(number){
-    return number % 2 === 0;
+
+
+// Define a function named convertDaysToHours that takes in one input Days. Return the conversion of the number of days into total hours. If the input is not numeric or a numeric string, convertDaysToHours, should return false.
+// convertDaysToHours(0) //0
+// convertDaysToHours('5') // 120
+// convertDaysToHours('Texas') //false
+// convertDaysToHours([2, 1, 0]) //false
+// convertDaysToHours(true) //false
+// convertDaysToHours(false) //false
+
+// [x] define(create) a function named convertDaysToHours with a parameter Days
+// [x] return the conversion of days into total hours
+// [] if the input is not numeric or a numeric string / return false
+// commit to git
+
+function convertDaysToHours(Days){
+    if(!isNaN(parseInt(Days)) && !isNaN(Days)){ //
+        return Days * 24;
+    } else {
+        return false;
+    }
 }
 
-function identity(input){
-    return input === input;
-}
-
-function isFive(input){
-    return input + 5;
-}
-
-function isMultipleOfFive(input){
-    return input * 5;
-}
-
-function isThree(input){
-    return input === 3;
-}
-
-function isMultipleOfThree(input){
-    return input * 3;
-}
 
 
-
+// Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent. If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
+// calculateTax(25, 8)				 // “$27.00"
+// calculateTax(10, 12)             // “$11.20”
+// calculateTax(120, 15.5)			 //	“$138.60"
+// calculateTax(10, true)           // false
+// calculateTax([1, 2, 3], 10)      // false
+// calculateTax(“Codeup”, 100)      // false
+// calculateTax()                   // false
 
 
 
